c007993a7acfc438eac16ca7f4a6d54caf5f02ac
Brynner Ventura <bventura03@hotmail.com>
2022-05-02 20:06:36 -0400

    commit

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import random
 import json
+
+from smash import Character, Battle
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+def character_look_up(name):
+    for x in characters:
+        if x["name"].lower() == name.lower():
+            return x
+        return None
+
+# Player picks their character
+def fight():
+    fighter = {}
+    comp = {}
+    choice = input('Choose your fighter =>')
+    if not choice:
+        choice = random.choice(characters)
+    print('You have chosen ' + choice)
+    fighter = character_look_up(choice)
+    comp = random.choice(characters)
+
+    player = Character(fighter["name"], 100, fighter["attacks"])
+    pc = Character(comp["name"], 100, comp["attacks"])
+    start = Battle(player, pc)
+    start.fight()
+    
+
+fight()
--- smash.py
+++ smash.py
@@ -1,1 +1,20 @@
 import random
+
+
+class Character:
+    def __init__(self, name, health, moves):
+        self.name = name
+        self.health = health
+        self.moves = moves 
+
+
+    def lower_health(self, damage):
+        self.health -= damage
+
+    def attack(self):
+        random.choice(self.moves)
+
+class Battle:
+    def __init__(self, player_one, player_two):
+        self.player_one = player_one
+        self.player_two = player_two
